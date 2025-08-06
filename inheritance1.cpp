c45bbf44dd5e6a0ead1c640c5d552b059e7207fb
bunraksa0106 <Bunraksastudent9d@gamil.com>
2025-08-07 06:03:45 +0700

    first commit

--- inheritance1.cpp
+++ inheritance1.cpp
@@ -20,14 +20,25 @@ class sample:public test{
     void inputall();
     void outputall();
     char* getdata2();
-    float getdata3();
+    float getdata3(){
+        return data3;
+    }
     void showdata();
     sample();
-    sample(int data1,char data2[25],int data3);
+    sample(char data2[25],int data3);
 };
+sample::sample(){
+    strcpy(data2,"ljdef");
+    data3=767;
+
+}
+sample::sample(char data2[25],int data3){
+    strcpy(this->data2,"Sarit");
+    this->data3=data3;
+}
 void test::input(){
     cout<<"\nEnter code: "; cin>>data1;
-    cout<<"\nEnter Name: "; cin.sync(); cin.get(data2,25);
+    cout<<"\nEnter Name: "; cin.ignore(); cin.getline(data2,25);
 }
 void test::output(){
     cout<<"\n"<<data1<<"\t"<<data2<<endl;
@@ -44,7 +55,19 @@ test::test(int data1,char data2[25]){
     this->data1=data1;
     strcpy(this->data2,"I m Bun Raksa");
 }
+void sample::outputall(){
+    cout<<"\nData2: "<<data2<<endl;
+    cout<<"\nValue data3: "<<data3<<endl;
+}
 int main(){
     cout<<"\nCreate data with OOP and Inheritance";
+    cout<<"\nInput data with base_class";
+    test obj;
+    obj.input();
+    obj.output();
+    getch();
+    sample obj1;
+    obj1.inputall();
+    obj1.outputall();
     return 0;
 }
