84bc1d1c57a28a3641683da8e95d2aa5bfbe950e
bunraksa0106 <Bunraksastudent9d@gamil.com>
2025-08-12 19:26:58 +0700

    first comit

---
+++ Practiceinheritance.cpp
@@ -0,0 +1,125 @@
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+#include<malloc.h>
+using namespace std;
+
+class Person{
+    private:
+    int code;
+    protected: 
+    char name[25];
+    public:
+    void input(); 
+    void output(); 
+    Person();
+    Person(int code=248, char *Name="Unknown");
+};
+class Worker: public Person{
+    private:
+    int hour; double salary;
+    protected:
+    static float Rate;
+    public:
+    void input();
+    void output();
+    static float getRate(){
+        return Rate;
+    }
+    friend void SearchName(Worker obj[100], int n, char Item[25]);
+    friend void SortSalary(Worker obj[100], int n);
+    Worker(int code=253,char *Name="Unknown", int hour=40, double salary=500);
+    static void Showall();
+};
+void Person::input(){
+    cout<<"\nEnter code: "; cin>>code;
+    cout<<"\nEnter Name: "; cin.ignore(); cin.getline(name, 25);
+}
+void Person::output(){
+    cout<<"\n"<<code;
+    cout<<"\t"<<name;
+}
+float Worker::Rate = 2.5;
+Person::Person(int code, char *Name){
+    this->code = code;
+    strcpy(name, Name);
+}
+Worker::Worker(int code, char *Name, int hour, double salary) : Person(code, Name) {
+    this->hour = hour;
+    this->salary = salary;
+}
+void Worker::input(){
+    Person::input();
+    cout<<"\nEnter Hour: "; cin>>hour;
+    cout<<"\nEnter Salary: "; cin>>salary;
+}
+void Worker::output(){
+    Person::output();
+    cout<<"\t"<<hour;
+    cout<<"\t"<<salary;
+}
+void SearchName(Worker obj[100], int n, char Item[25]){
+    int found = 0;
+    for(int i = 0; i < n; i++){
+        if(strcmp(obj[i].name, Item) == 0){
+            cout<<"\nData after Search: ";
+            obj[i].output();
+            found = 1;
+        }
+    }
+    if(found==0) 
+    cout<<"\nNot Found!";
+}
+void SortSalary(Worker obj[100], int n){
+    Worker temp; int i,j;
+    for(i=0;i<n;i++){
+        for(j = i+1; j<n;j++){
+            if(obj[i].salary>obj[j].salary){
+                temp = obj[i];
+                obj[i] = obj[j];
+                obj[j] = temp;
+            }
+        }
+    }
+    cout<<"\nData After sort";
+    cout<<"\nCode\tName\tHour\tSalary";
+    for(int i = 0; i < n; i++){
+        obj[i].output();
+    }
+}
+int main(){
+    cout<<"\nPractice with Inheritance";
+    Worker obj[100];
+    int n, choice;
+    char Item[25];
+    cout<<"\nEnter Number of Workers: "; cin>>n;
+    for(int i = 0; i < n; i++){
+        cout<<"\nEnter Details of Worker "<<i+1<<": ";
+        obj[i].input();
+    }
+    do{
+        cout<<"\n1. Search by Name";
+        cout<<"\n2. Sort by Salary";
+        cout<<"\n3. Show All Workers";
+        cout<<"\n4. Exit";
+        cout<<"\nEnter your choice: "; cin>>choice;
+        switch(choice){
+            case 1:
+                cout<<"\nEnter Name to Search: "; cin.ignore(); cin.getline(Item, 25);
+                cout<<"\nCode\tName\tHour\tSalary";
+                SearchName(obj, n, Item);
+                break;
+            case 2:
+                SortSalary(obj, n);
+                break;
+            case 3:
+                // Worker::Showall();
+                break;
+            case 4:
+                cout<<"\nExiting...";
+                break;
+            default:
+                cout<<"\nInvalid Choice!";}
+        }while(choice!=4);
+    return 0;
+}
--- inheritance1.cpp
+++ inheritance1.cpp
@@ -15,7 +15,7 @@ class test{
 };
 class sample:public test{
     private:
-    int data3; int code;
+    int data3, code;
     public:
     void inputall();
     void outputall();
@@ -32,8 +32,20 @@ class sample:public test{
 sample::sample(){
     data3=767;
 }
+void sortSample(){
+    int n,i,j;
+    sample obj[100],temp;
+    for(i=0;i<n;i++){
+        for(i=i+1;j<n;j++){
+            if(obj[i].getdata3()>obj[j].getdata3())
+               temp=obj[i].getdata3()
+               obj[i].getdata3()=obj[j].getdata3()
+               obj[j].getdata3()=temp;
+        }
+    }
+}
 sample::sample(int code,char data2[25],int data3){
-    strcpy(this->data2,"Sarit");
+    strcpy(this->data2,data2);
     this->data3=data3;
     this->code=code;
 }
@@ -42,42 +54,49 @@ void test::input(){
     cout<<"\nEnter Name: "; cin.ignore(); cin.getline(data2,25);
 }
 void test::output(){
-    cout<<"\n"<<data1<<"\t"<<data2<<endl;
+    cout<<"\n"<<data1<<"\t"<<data2;
 }
 void sample::inputall(){
     input();
     cout<<"\nEnter data3: "; cin>>data3;
+    cout<<"\nEnter code: "; cin>>code;
+}
+void head(){
+    cout<<"\nID\tName";
 }
 test::test(){
-    data1=90;
+    data1=98;
     strcpy(data2,"Bun Raksa");
 }
 test::test(int data1,char data2[25]){
     this->data1=data1;
-    strcpy(this->data2,"I m Bun Raksa");
+    strcpy(this->data2,data2);
 }
 void sample::outputall(){
-    cout<<"\nData2: "<<data2<<endl;
-    cout<<"\nValue data3: "<<data3<<endl;
+    cout<<"\n"<<data2<<endl;
+    cout<<"\t"<<data3<<endl;
+    cout<<"\t"<<code<<endl;
 }
 int main(){
     test obj11; test obj12(335,"Test");
     cout<<"\nCreate data with OOP and Inheritance";
     cout<<"\n1.1.Output object of constructor ";
-    obj11.output();  obj12.output();
+    head();
+    obj11.output();  obj12.output(); getch();
     cout<<"\n1.1.input one object of base class";
-    obj11.input(); obj12.input();
+    obj11.input(); 
     cout<<"\n2.Create object with derived class ";
-    cout<<"\n2.1.Use constructors "; sample obj21; sample obj22(56,"Samnang",98);
+    cout<<"\n2.1.Use constructors "; 
+    sample obj21; sample obj22(56,"Samnang",98);
+    head();
     obj21.outputall(); obj22.outputall();
     cout<<"\n2.2.Input one object of base class ";
-    obj21.inputall();
+    obj21.inputall(); getch();
     obj21.outputall();
-    cout<<"\n=====================================";
     getch();
     cout<<"\n3.Create list with inpheritance \n";
     cout<<"\n3.1.Interlize 7 objects and output on screen ";
-    int n=7;
+    int n; n=7;
     int i;
     sample Aobj[7]={sample(78,"Hengkim",99),sample(),sample(278,"Um Rithy",17),sample(89,"Neang Saray",27),sample(892,"Nori",77),sample(194,"PengKry",11),sample(829,"Makara",79)};
     cout<<"\nOutput on screen\n";
